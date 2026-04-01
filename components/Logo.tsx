580051bb3b09b30230d30b641a879c12c809f6e3
SammyGilis <sgilis1029@gmail.com>
2026-03-31 22:28:48 -0400

    update logo to new P with leaves design

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,13 +1,18 @@
 export function Logo() {
   return (
     <svg viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ width: 36, height: 36 }}>
-      <circle cx="18" cy="18" r="18" fill="#16a34a" />
-      <rect x="8" y="6" width="6" height="24" rx="2" fill="white" />
-      <path d="M14 6 C14 6 26 6 26 13 C26 20 14 20 14 20 L14 6Z" fill="white" />
-      <path d="M14.5 9 C14.5 9 22.5 9 22.5 13 C22.5 17 14.5 17 14.5 17 L14.5 9Z" fill="#16a34a" />
-      <path d="M13 6.5 C12 4.5 9.5 3.5 9 2.5 C9 2.5 10.5 2 12 3.5 C13 4.5 13 6 13 6.5Z" fill="#16a34a" />
-      <path d="M14.5 6.5 C15.5 4.5 18 3.5 18.5 2.5 C18.5 2.5 17 2 15.5 3.5 C14.5 4.5 14.5 6 14.5 6.5Z" fill="#22c55e" />
-      <rect x="13.5" y="5.5" width="1" height="3" rx="0.5" fill="#15803d" />
+      {/* Green P letter */}
+      <rect x="5" y="4" width="7" height="28" rx="3.5" fill="#2d8c1e"/>
+      {/* P bump (top half circle) */}
+      <path d="M12 4 C12 4 26 4 26 11.5 C26 19 12 19 12 19 L12 4Z" fill="#2d8c1e"/>
+      {/* Inner cutout of P */}
+      <path d="M12.5 7.5 C12.5 7.5 21.5 7.5 21.5 11.5 C21.5 15.5 12.5 15.5 12.5 15.5 L12.5 7.5Z" fill="white"/>
+      {/* Left leaf */}
+      <path d="M11 4.5 C10 2.5 7.5 2 7 1 C7 1 7.5 0.5 9 1.5 C10.5 2.5 11 4 11 4.5Z" fill="#2d8c1e"/>
+      {/* Right leaf */}
+      <path d="M13 4.5 C14 2.5 16.5 2 17 1 C17 1 16.5 0.5 15 1.5 C13.5 2.5 13 4 13 4.5Z" fill="#3aaa22"/>
+      {/* Leaf stem dot */}
+      <circle cx="12" cy="4.2" r="0.8" fill="#1a6b10"/>
     </svg>
   );
 }
